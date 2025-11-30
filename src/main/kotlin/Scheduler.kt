e0d759936b1c702aac487028c65036ded7b6b18b
Fedor Zabello <153311587+fedor-zabello@users.noreply.github.com>
2025-11-30 14:38:56 +0300

    Mask bot behavior (#2)
    * - added userAgent, referrer, etc. for client
    - added jitter for scheduler
    - don't call spbhl twice in scheduler
    
    * kls_database in gitignore

--- src/main/kotlin/Scheduler.kt
+++ src/main/kotlin/Scheduler.kt
@@ -1,3 +1,8 @@
+import java.time.LocalDate
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.Duration.Companion.minutes
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
@@ -7,8 +12,7 @@ import model.SpbhlMatch
 import service.MatchService
 import service.NotificationService
 import util.DateTimeUtils
-import java.time.LocalDate
-import java.util.concurrent.ConcurrentHashMap
+import kotlin.time.Duration.Companion.milliseconds
 
 class Scheduler(
     private val matchService: MatchService,
@@ -17,26 +21,33 @@ class Scheduler(
     private val scheduledForNotify = ConcurrentHashMap.newKeySet<SpbhlMatch>()
     private val scheduledForCheckResult = ConcurrentHashMap.newKeySet<SpbhlMatch>()
 
-    fun start() = CoroutineScope(Dispatchers.Default).launch {
-        runNotificationChecker()
-        println("Scheduler is started")
-    }
+    fun start() =
+        CoroutineScope(Dispatchers.Default).launch {
+            runNotificationChecker()
+            println("Scheduler is started")
+        }
 
     private suspend fun runNotificationChecker() = coroutineScope {
         delay(5 * 1000L) // Initial delay
         while (true) {
             println("Checking upcoming matches on spbhl...")
-            launch { scheduleNotifications() }
-            launch { scheduleScoreChecks() }
-            delay(3 * 60 * 60 * 1000) // Check every 3 hours
+            val upcomingMatches = matchService.getAllUpcoming()
+            launch { scheduleNotifications(upcomingMatches) }
+            launch { scheduleScoreChecks(upcomingMatches) }
+
+            val jitter = (Random.nextLong(30.minutes.inWholeMilliseconds)).milliseconds
+            val delay = 6.hours + jitter
+            println(delay)
+            delay(delay)
         }
     }
 
-    private suspend fun scheduleNotifications() = coroutineScope {
+    private suspend fun scheduleNotifications(upcomingMatches: List<SpbhlMatch>) = coroutineScope {
         println("Checking upcoming matches for notifications")
-        matchService.getAllUpcoming()
+        upcomingMatches
             .filter { match ->
-                match.date.toLocalDate() == LocalDate.now().plusDays(1) && !scheduledForNotify.contains(match)
+                match.date.toLocalDate() == LocalDate.now().plusDays(1) &&
+                        !scheduledForNotify.contains(match)
             }
             .forEach { match ->
                 val notificationDelay = DateTimeUtils.calculateDelayForNotification(match.date)
@@ -47,20 +58,25 @@ class Scheduler(
                         try {
                             notificationService.notifyForUpcomingMatch(match)
                         } catch (e: Exception) {
-                            notificationService.notifyAdmin("Error while trying to notify for upcoming match. Match $match. Error: ${e.message}")
+                            notificationService.notifyAdmin(
+                                "Error while trying to notify for upcoming match. Match $match. Error: ${e.message}"
+                            )
                         }
                         scheduledForNotify.remove(match)
                     }
-                    println("Scheduled notification for match ${match.teams} at ${match.date} with delay: $notificationDelay ms")
+                    println(
+                        "Scheduled notification for match ${match.teams} at ${match.date} with delay: $notificationDelay ms"
+                    )
                 }
             }
     }
 
-    private suspend fun scheduleScoreChecks() = coroutineScope {
+    private suspend fun scheduleScoreChecks(upcomingMatches: List<SpbhlMatch>) = coroutineScope {
         println("Checking today's matches for score updates")
-        matchService.getAllUpcoming()
+        upcomingMatches
             .filter { match ->
-                match.date.toLocalDate() == LocalDate.now() && !scheduledForCheckResult.contains(match)
+                match.date.toLocalDate() == LocalDate.now() &&
+                        !scheduledForCheckResult.contains(match)
             }
             .forEach { match ->
                 val resultCheckDelay = DateTimeUtils.calculateDelayForScoreCheck(match.date)
@@ -72,11 +88,15 @@ class Scheduler(
                         try {
                             checkForScoreUpdates(match)
                         } catch (e: Exception) {
-                            notificationService.notifyAdmin("Error while trying to check and send match score. Match $match. Error: ${e.message}")
+                            notificationService.notifyAdmin(
+                                "Error while trying to check and send match score. Match $match. Error: ${e.message}"
+                            )
                         }
                         scheduledForCheckResult.remove(match)
                     }
-                    println("Scheduled result check for match ${match.teams} at ${match.date} with delay: $resultCheckDelay ms")
+                    println(
+                        "Scheduled result check for match ${match.teams} at ${match.date} with delay: $resultCheckDelay ms"
+                    )
                 }
             }
     }
@@ -84,15 +104,19 @@ class Scheduler(
     private suspend fun checkForScoreUpdates(match: SpbhlMatch) {
         var scoreDiscovered = false
         while (!scoreDiscovered) {
-            matchService.getAll().firstOrNull { updatedMatch ->
-                updatedMatch.teams == match.teams
-                        && updatedMatch.date == match.date
-                        && updatedMatch.score != null
-            }?.let { updatedMatch ->
-                notificationService.notifyForResult(updatedMatch)
-                scoreDiscovered = true
-            }
-            delay(5 * 60 * 1000) // Check every 5 minutes
+            matchService.getAll()
+                .firstOrNull { updatedMatch ->
+                    updatedMatch.teams == match.teams &&
+                            updatedMatch.date == match.date &&
+                            updatedMatch.score != null
+                }
+                ?.let { updatedMatch ->
+                    notificationService.notifyForResult(updatedMatch)
+                    scoreDiscovered = true
+                }
+            val delay = 5.minutes
+            val jitter = (Random.nextLong(4.minutes.inWholeMilliseconds)).milliseconds
+            delay(delay + jitter)
         }
     }
 }
--- src/main/kotlin/service/SpbhlClient.kt
+++ src/main/kotlin/service/SpbhlClient.kt
@@ -2,13 +2,15 @@ package service
 
 import model.SpbhlMatchDto
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 class SpbhlClient(
-    private val spbhlUrl: String = "https://spbhl.ru",
-    private val teamIds: List<String> = listOf(
-        "b82f09ec-bf65-4d64-881f-0bef1598d936",
-        "dc079cd6-d6e2-4b12-b7fa-b8676195a081"
-    ),
+        private val spbhlUrl: String = "https://spbhl.ru",
+        private val teamIds: List<String> =
+                listOf(
+                        "b82f09ec-bf65-4d64-881f-0bef1598d936",
+                        "dc079cd6-d6e2-4b12-b7fa-b8676195a081"
+                ),
 ) {
 
     fun getAllMatches(): List<SpbhlMatchDto> {
@@ -17,28 +19,39 @@ class SpbhlClient(
         try {
             teamIds.forEach { teamId ->
                 val url = "$spbhlUrl/Schedule?TeamID=$teamId"
-                val document = Jsoup.connect(url).get()
+                val document = allMatchesFromSpbhl(url)
                 val matchTable = document.select("#MatchGridView")
                 val matchRows = matchTable.select("tr")
 
-                matchDtoList.addAll(matchRows.map {
-                    val tournament = it.select("td:nth-child(1)").text()
-                    val date = it.select("td:nth-child(4)").text()
-                    val time = it.select("td:nth-child(5)").text()
-                    val arena = it.select("td:nth-child(6)").text()
-                    val teams = it.select("td:nth-child(7)").text()
-                    val score = it.select("td:nth-child(8)").text()
-                    SpbhlMatchDto(tournament, date, time, arena, teams, score)
-                })
+                matchDtoList.addAll(
+                        matchRows.map {
+                            val tournament = it.select("td:nth-child(1)").text()
+                            val date = it.select("td:nth-child(4)").text()
+                            val time = it.select("td:nth-child(5)").text()
+                            val arena = it.select("td:nth-child(6)").text()
+                            val teams = it.select("td:nth-child(7)").text()
+                            val score = it.select("td:nth-child(8)").text()
+                            SpbhlMatchDto(tournament, date, time, arena, teams, score)
+                        }
+                )
             }
         } catch (e: Exception) {
             println("ERROR: error while getting matches from spbhl. ${e.message}")
         }
 
         return matchDtoList.filter { matchDto ->
-            matchDto.teams.isNotEmpty()
-                    && matchDto.date.isNotEmpty()
-                    && matchDto.time.isNotEmpty()
+            matchDto.teams.isNotEmpty() && matchDto.date.isNotEmpty() && matchDto.time.isNotEmpty()
         }
     }
+
+    private fun allMatchesFromSpbhl(url: String): Document {
+        return Jsoup.connect(url)
+            .userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0 Safari/537.36")
+            .referrer("https://google.com")
+            .timeout(10000)
+            .followRedirects(true)
+            .ignoreContentType(true)
+            .get()
+    }
+
 }
